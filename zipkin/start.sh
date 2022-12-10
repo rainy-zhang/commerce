6c4f4fdeea5b2277eab63d9d95c6856fbe295a0a
rainy-zhang <791136444@qq.com>
2022-12-10 16:00:38 +0800

    remove kafka soft

--- zipkin/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-nohup java -DKAFKA_BOOTSTRAP_SERVERS=localhost:9092 -DKAFKA_TOPIC=zipkin -jar zipkin-server-2.23.19-exec.jar --STORAGE_TYPE=mysql --MYSQL_HOST=localhost --MYSQL_TCP_PORT=3306 --MYSQL_DB=e-commerce-zipkin --MYSQL_USER=root --MYSQL_PASS=12345678 >> zipkin.log &
